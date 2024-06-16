d3e0826f7b768433ad0a8108983c682d26a8928f
the2cguy <the2cguy@proton.me>
2024-06-16 20:45:57 +0700

    Finish Timer (Break not included)

--- script.js
+++ script.js
@@ -1,27 +1,93 @@
 var a;
 var mode = "time"
-var currentState = "main"
+var currentState = "none"
+var paused = false
+var seconds = 0 
+function stp(){
+    clearInterval(a);
+    currentState = "none"
+    $(".startbtn").html('<i class="bi bi-play-fill"></i> Start Pomodoro!');
+    if(mode=="money"){
+        $(".main-clock").text("$0");
+        $(".mini").text("00:00");
+    }else{
+        $(".main-clock").text("00:00");
+        $(".mini").text("$0");
+    }
+    $(".stop").hide()
+}
+function timerend(){
+
+}
+function timetodollar(seconds){
+    var mins = seconds/60
+    var dollars = mins * 0.05
+    return dollars
+}
+function getTimeTime(){
+    return Math.floor(seconds/60).toString().padStart(2, '0')+":"+Math.floor(seconds%60).toString().padStart(2, '0');
+}
 $(".intime").click(function (e) { 
     e.preventDefault();
-    if ($(".intime").text() == "In Money"){
-        $(".intime").text("In Time");
+    if ($(".intime").text() == "In Time"){
+        $(".intime").text("In Money");
+        $(".main-clock").text("00:00");
+        $(".mini").text("$0");
+        if(currentState == "main" || currentState == "pause"){
+            $(".main-clock").text(getTimeTime());
+            $(".mini").text("$"+timetodollar(seconds).toFixed(3).toString());
+        }
         mode = "time"
     }else{
-        $(".intime").text("In Money");
+        $(".intime").text("In Time");
         mode = "money"
+        $(".main-clock").text("$0");
+        $(".mini").text("00:00");
+        if(currentState == "main" || currentState == "pause"){
+            $(".mini").text(getTimeTime());
+            $(".main-clock").text("$"+timetodollar(seconds).toFixed(3).toString());
+        }
     }
 });
-$(".startbtn").click(function (e) { 
-    currentState = "main"
-    clearInterval(a)
+$(".stop").click(function (e) { 
     e.preventDefault();
-    var targettime = moment()
-    targettime.add($("#preset").val().split("-")[0], "minutes");
-    a = setInterval(() => {
-        $(".main-clock").text(targettime.diff(moment(), "minutes").toString()+":"+(targettime.diff(moment(), "seconds")%60));
-        if(targettime.diff(moment(), "seconds") < 1){
-            alert("yay!")
-            clearInterval(a);
-        }
-    }, 100)
+    stp();
+});
+$(".startbtn").click(function (e) { 
+    if (currentState == "none"){
+        $(".stop").show()
+
+        currentState = "main"
+        $(".startbtn").html('<i class="bi bi-pause-fill"></i> Pause');
+        clearInterval(a)
+        e.preventDefault();
+        seconds = $("#preset").val().split("-")[0] * 60
+        a = setInterval(() => {
+            if (currentState != "pause"){
+                if(mode == "time"){
+                    $(".main-clock").text(getTimeTime());
+                    $(".mini").text("$"+timetodollar(seconds).toFixed(3).toString());
+                }else{
+                    $(".main-clock").text("$"+timetodollar(seconds).toFixed(3).toString());
+                    $(".mini").text(getTimeTime());
+                }
+                if(seconds < 1){
+                    timerend()
+                    clearInterval(a);
+                    stp()
+                }
+                seconds -= 0.1
+            }
+        }, 100)
+    }
+    else if(currentState == "main"){
+        currentState = "pause"
+        paused = true
+        $(".startbtn").html('<i class="bi bi-play-fill"></i> Resume Pomodoro!');
+    }
+    else if(currentState == "pause"){
+        currentState = "main";
+        paused = false
+        $(".startbtn").html('<i class="bi bi-pause-fill"></i> Pause');
+    }
 });
